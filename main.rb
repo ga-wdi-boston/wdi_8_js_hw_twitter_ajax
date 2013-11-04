8e6af6df8f18043654cb9d0cc74d3931d7bc7593
Rahul Horé <hore.rahul@gmail.com>
2013-11-04 17:42:43 -0500

    Updates main.rb

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ require 'twitter'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'sinatra/activerecord'
-require_relative 'config/twitter'
+# require_relative 'config/twitter'
 
 Dotenv.load
 
@@ -16,20 +16,11 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV["ACCESS_SECRET"]
 end
 
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-class Tweejax < ActiveRecord::Base
-end
-
-# Gets my 20 recent tweets text
-get '/recent' do
-  client.user_timeline.map(&:text)
-  binding.pry
-end
-
-# Gets :user's 20 recent tweets text
+# Gets :user's 20 most recent tweets text
 get '/recent/:user' do
-  client.user_timeline(params[:user]).map(&:text)
+  client.user_timeline(params[:user]).map(&:text).to_json
 end
 
 
