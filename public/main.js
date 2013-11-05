817858eac957007469bd64d90d49d01980760f55
Cori Allen <coriallen@gmail.com>
2013-11-05 17:55:29 -0500

    Update render tweets method with jquery.

--- public/main.js
+++ public/main.js
@@ -6,7 +6,7 @@ var setButtonEvent = function() {
 	});
 };
 
-var get_user_input = function(username) {
+var get_user_input = function() {
 	var search_box = document.getElementById("search-box");
 	var screen_name = search_box.value;
 	async_load_json(screen_name);
@@ -18,7 +18,7 @@ var async_load_json = function(screen_name) {
 	xhr.onload = function(e) {
 		if(xhr.readyState === 4){
 			if(xhr.status === 200){
-			var parsed_json = JSON.parse(xhr.responseText);
+			var parsed_json = xhr.responseText;
 			render_tweets(parsed_json);
 		} else {
 			console.error(xhr.statusText);
@@ -30,19 +30,21 @@ var async_load_json = function(screen_name) {
 
 };
 
-var render_tweets = function(parsed_json) {
-	var json_length,
-		tweet,
-		tweet_list,
-		i = 0;
-	json_length = parsed_json.length;
-	tweet_list = document.getElementById('tweet-list');
-	tweet_list.innerHTML = '';
-	for(;i < json_length;) {
-		tweet = parsed_json[i];
-		tweet_list.innerHTML += ('<li>' + tweet + '</li>');
-		i = i + 1;
-	}
+var render_tweets = function(parced_json) {
+	var tweets = $.parseJSON(parced_json);
+	console.log(tweets);
+	// var json_length,
+	// 	tweet,
+	// 	tweet_list,
+	// 	i = 0;
+	// json_length = parsed_json.length;
+	// tweet_list = document.getElementById('tweet-list');
+	// tweet_list.innerHTML = '';
+	// for(;i < json_length;) {
+	// 	tweet = parsed_json[i];
+	// 	tweet_list.innerHTML += ('<li>' + tweet + '</li>');
+	// 	i = i + 1;
+	// }
 };
 
 window.onload = function() {setButtonEvent();};
