deb9beab60fd0084c8d14a6a84bb1a2dd2677526
Abby Howell <abby.g.howell@gmail.com>
2013-11-05 17:59:21 -0500

    mostly transitioned to jquery

--- public/main.js
+++ public/main.js
@@ -7,38 +7,51 @@ var setButtonEvent = function() {
   });
 };
 
-var get_user_input = function(username) {
+var get_user_input = function() {
 	var search_box = $('#search-box')[0];
   var screen_name = search_box.value;
-  async_load_json(screen_name);
+  load_json(screen_name);
 };
 
-var async_load_json = function(screen_name) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', "/tweets/" + screen_name, true);
-  xhr.onload = function(e) {
-    if(xhr.readyState === 4) {
-      if(xhr.status === 200) {
-        var parsed_json = JSON.parse(xhr.responseText);
-        render_tweets(parsed_json);
-      } else {
-      	console.error(xhr.statusText);
-      }
+var load_json = function(screen_name) {
+  $.ajax({
+    url: "/tweets/" + screen_name,
+    data: {
+      username: screen_name
+    },
+    success: function(data) {
+      render_tweets(data);
     }
-  };
-  xhr.send(null);
+  });
 };
 
-var render_tweets = function(parsed_json) {
+// var async_load_json = function(screen_name) {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('GET', "/tweets/" + screen_name, true);
+//   xhr.onload = function(e) {
+//     if(xhr.readyState === 4) {
+//       if(xhr.status === 200) {
+//         var parsed_json = JSON.parse(xhr.responseText);
+//         render_tweets(parsed_json);
+//       } else {
+//       	console.error(xhr.statusText);
+//       }
+//     }
+//   };
+//   xhr.send(null);
+// };
+
+var render_tweets = function(tweet_string) {
 	var json_length,
 			tweet,
 			tweet_list,
 			i = 0;
-	json_length = parsed_json.length;
-	tweet_list = document.getElementById('tweet-list');
+  var tweets = $.parseJSON(tweet_string)
+	json_length = tweets.length;
+	tweet_list = $('#tweet-list')[0];
 	tweet_list.innerHTML = '';
 	for(;i < json_length;) {
-		tweet = parsed_json[i];
+		tweet = tweets[i];
 		tweet_list.innerHTML += ('<li>' + tweet + '</li>');
 		i = i + 1;
 	}
