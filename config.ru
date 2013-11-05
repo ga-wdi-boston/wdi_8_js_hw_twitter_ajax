cce18bec1a396e2d8e9dda8bc00869e40260c2e3
Blake Ruddock <ruddock.blake@gmail.com>
2013-11-05 17:35:55 -0500

    Converted AJAX to jquery

--- config.ru
+++ config.ru
@@ -1,8 +1,2 @@
 require './main'
 run Sinatra::Application
-require 'sprockets'
-
-environment = Sprockets::Environment.new
-environment.append_path 'public/main.js'
-run environment
-end
--- public/main.js
+++ public/main.js
@@ -1,45 +1,27 @@
-window.onload = function() {
-  var submit_button = document.getElementById('submit');
-
-  submit_button.addEventListener('click', function(event) {
+$(document).ready(function() {
+  $('#submit').click(function(event){
     event.preventDefault();
-    submitButtonResponse();
+    ajaxRequest($('#handle-field').val());
   });
-
-  submitButtonResponse = function() {
-    var handleform = document.getElementById('handle-field')
-    var handle = handleform.value;
-    ajaxRequest(handle);
-  };
-};
-
+});
 
 var render_tweets = function(tweets_json) {
-  var tweets_json = tweets_json;
+  var tweets_array = $.parseJSON(tweets_json)
   tweets_html = ""
   i = 0;
-  for (; i < tweets_json.length; i++){
-    var tweet_text = tweets_json[i];
+  for (; i < tweets_array.length; i++){
+    var tweet_text = tweets_array[i];
     tweets_html += "<li>" + tweet_text + "</li>"
   };
-  document.getElementById('tweets').innerHTML = tweets_html;
+  $('#tweets').append(tweets_html);
 };
 
-
-
 var ajaxRequest = function(user) {
-  var request = new XMLHttpRequest();
   var url = '/twitter/' + user;
-  request.open("GET", url, true);
-  request.onload = function(e) {
-    if(request.readyState === 4) {
-      if(request.status === 200){
-        parsed_json = JSON.parse(request.responseText);
-        render_tweets(parsed_json);
-      } else {
-        console.error(request.statusText);
-      };
-    };
-  };
-  request.send(null);
+  $.ajax({
+    url: url,
+    success: function(data) {
+      render_tweets(data);
+    } 
+  });
 };
