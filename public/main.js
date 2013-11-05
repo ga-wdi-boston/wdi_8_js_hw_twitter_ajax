2ffb0f7adb6dc2faeccb20877cf981c6bf8bb16d
Cori Allen <coriallen@gmail.com>
2013-11-05 18:36:01 -0500

    refactored async load json and get user input with json & ajax.

--- public/main.js
+++ public/main.js
@@ -1,49 +1,40 @@
 var setButtonEvent = function() {
-$("#search-button").click(function(event){
-		event.preventDefault();
+$("#search-button").click(function(e){
+		e.preventDefault();
 		get_user_input();
 	});
 };
 
 var get_user_input = function() {
-	var search_box = document.getElementById("search-box");
-	var screen_name = search_box.value;
-	async_load_json(screen_name);
+	async_load_json($("#search-box").val());
 };
 
 var async_load_json = function(screen_name) {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', "/tweets/"+ screen_name, true);
-	xhr.onload = function(e) {
-		if(xhr.readyState === 4){
-			if(xhr.status === 200){
-			var parsed_json = xhr.responseText;
-			render_tweets(parsed_json);
-		} else {
-			console.error(xhr.statusText);
+	$.ajax( {
+		url: "/tweets/" + screen_name,
+		success: function(data) {
+			render_tweets(data);
 		}
+	});
+};
+
+
+var render_tweets = function(data) {
+	var tweets = $.parseJSON(data);
+	var json_length,
+		tweet,
+		tweet_list,
+		i = 0;
+	json_length = tweets.length;
+	tweet_list = document.getElementById('tweet-list');
+	tweet_list.innerHTML = '';
+	for(;i < json_length;) {
+		tweet = tweets[i];
+		tweet_list.innerHTML += ('<li>' + tweet + '</li>');
+		i = i + 1;
 	}
 };
 
-	xhr.send(null);
-
-};
-
-var render_tweets = function(parced_json) {
-	var tweets = $.parseJSON(parced_json);
-	console.log(tweets);
-	// var json_length,
-	// 	tweet,
-	// 	tweet_list,
-	// 	i = 0;
-	// json_length = parsed_json.length;
-	// tweet_list = document.getElementById('tweet-list');
-	// tweet_list.innerHTML = '';
-	// for(;i < json_length;) {
-	// 	tweet = parsed_json[i];
-	// 	tweet_list.innerHTML += ('<li>' + tweet + '</li>');
-	// 	i = i + 1;
-	// }
-};
-
-window.onload = function() {setButtonEvent();};
+$(function() {
+  setButtonEvent();
+});
