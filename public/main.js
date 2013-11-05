05c8cc83e789d42d4530423bb0a160a1aea2946e
Ben Winter <ben.g.winter@gmail.com>
2013-11-05 17:46:40 -0500

    finsihed transitioning ot jquery

--- public/main.js
+++ public/main.js
@@ -1,17 +1,10 @@
-window.onload = function() {
-  var submit_button = document.getElementById('submit');
-
-  submit_button.addEventListener('click', function(event) {
+$(document).ready(function() {
+  $('#submit').click(function(event){
     event.preventDefault();
-    submitButtonResponse();
+    ajaxRequest($('#handle-field').val());
   });
+});
 
-  submitButtonResponse = function() {
-    var handleform = document.getElementById('handle-field')
-    var handle = handleform.value;
-    ajaxRequest(handle);
-  };
-};
 
 var render_tweets = function(json) {
   var tweet_html = "";
@@ -24,20 +17,14 @@ var render_tweets = function(json) {
     };
     document.getElementById('tweets-list').innerHTML = tweet_html;
 };
-  
+
 var ajaxRequest = function(user) {
-  var request = new XMLHttpRequest();
   var url = '/twitter/' + user;
-  request.open("GET", url, true);
-  request.onload = function(e) {
-    if(request.readyState === 4) {
-      if(request.status === 200){
-        parsed_json = JSON.parse(request.responseText);
-        render_tweets(parsed_json);
-      } else {
-        console.error(request.statusText);
-      };
-    };
-  };
-  request.send(null);
+  $.ajax({
+    dataType: "json",
+    url: url,
+    success: function(data) {
+      render_tweets(data);
+  } 
+  });
 };
