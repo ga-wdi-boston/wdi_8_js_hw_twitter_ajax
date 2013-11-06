ab045935f817f4c95bb57e549ca70dad49561bd7
Rahul Horé <hore.rahul@gmail.com>
2013-11-06 07:13:19 -0500

    Implements AJAX jQuery request

--- public/main.js
+++ public/main.js
@@ -1,5 +1,4 @@
 $(function() {
-  
   $('#submit-button').click(function(event) {
     event.preventDefault();
     ajax_request($('#username-field').val());
@@ -7,33 +6,44 @@ $(function() {
 });
 
 var ajax_request = function(user) {
-  var xhr = new XMLHttpRequest();
-  url = '/recent/' + user;
-  xhr.open('GET', url, true);
-  xhr.onload = function(e) {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        parsed_json = JSON.parse(xhr.responseText);
-        render_tweets(parsed_json);
-      } else {
-        console.error(xhr.statusText);
-      }
+  //var xhr = new XMLHttpRequest();
+  var url = '/recent/' + user;
+  $.ajax({
+    url: url,
+    success: function(data) {
+      render_tweets(data);
     }
-  };
-  xhr.send(null);
+  });
+  // xhr.open('GET', url, true);
+  // xhr.onload = function(e) {
+  //   if (xhr.readyState === 4) {
+  //     if (xhr.status === 200) {
+  //       parsed_json = JSON.parse(xhr.responseText);
+  //       render_tweets(parsed_json);
+  //     } else {
+  //       console.error(xhr.statusText);
+  //     }
+  //   }
+  // };
+  // xhr.send(null);
 };
 
 var render_tweets = function(tweets_json) {
-  var tweets_html = "", 
+  var tweets = $.parseJSON(tweets_json),
+      tweets_html = "",
       index = 0, 
-      length = tweets_json.length,
-      tweet_list;
+      length = tweets.length;
+      //tweet_list;
 
-  document.getElementById('tweenty-tweets').innerHTML = "";
-  tweet_list = document.getElementById('tweenty-tweets');
+  // document.getElementById('tweenty-tweets').innerHTML = "";
+  // $('#tweenty-tweets').innerHTML = "";
+  // tweet_list = document.getElementById('tweenty-tweets');
+  // tweet_list = $('#tweenty-tweets');
 
   for (; index < length; ) {
-    tweet_list.innerHTML += "<li>" + tweets_json[index] + "</li>";
+    // $('#tweenty-tweets').innerHTML += "<li>" + tweets_json[index] + "</li>";
+    tweets_html += "<li>" + tweets[index] + "</li>";
     index += 1;
   }
+  $("#tweenty-tweets").append(tweets_html);
 };
