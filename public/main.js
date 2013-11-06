984ff5bf24103b233d30e803a8372980011e21f9
Rahul Horé <hore.rahul@gmail.com>
2013-11-06 07:23:02 -0500

    Cleans up main.js

--- public/main.js
+++ public/main.js
@@ -6,7 +6,6 @@ $(function() {
 });
 
 var ajax_request = function(user) {
-  //var xhr = new XMLHttpRequest();
   var url = '/recent/' + user;
   $.ajax({
     url: url,
@@ -14,18 +13,6 @@ var ajax_request = function(user) {
       render_tweets(data);
     }
   });
-  // xhr.open('GET', url, true);
-  // xhr.onload = function(e) {
-  //   if (xhr.readyState === 4) {
-  //     if (xhr.status === 200) {
-  //       parsed_json = JSON.parse(xhr.responseText);
-  //       render_tweets(parsed_json);
-  //     } else {
-  //       console.error(xhr.statusText);
-  //     }
-  //   }
-  // };
-  // xhr.send(null);
 };
 
 var render_tweets = function(tweets_json) {
@@ -33,15 +20,10 @@ var render_tweets = function(tweets_json) {
       tweets_html = "",
       index = 0, 
       length = tweets.length;
-      //tweet_list;
 
-  // document.getElementById('tweenty-tweets').innerHTML = "";
-  // $('#tweenty-tweets').innerHTML = "";
-  // tweet_list = document.getElementById('tweenty-tweets');
-  // tweet_list = $('#tweenty-tweets');
+  $("#tweenty-tweets").empty();
 
   for (; index < length; ) {
-    // $('#tweenty-tweets').innerHTML += "<li>" + tweets_json[index] + "</li>";
     tweets_html += "<li>" + tweets[index] + "</li>";
     index += 1;
   }
