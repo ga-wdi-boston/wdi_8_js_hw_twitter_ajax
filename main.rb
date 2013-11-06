1b6e627cc1f05a6015742f8c328dc71df10e7da2
Ryan Page <rpage326@gmail.com>
2013-11-05 19:58:00 -0500

    AJAX to jQuery

--- main.rb
+++ main.rb
@@ -1,33 +1,28 @@
-require 'twitter'
+
+
+require 'sinatra'
+require 'sinatra/reloader'
 require 'pry'
 require 'dotenv'
-require 'sinatra'
-require 'sinatra/reloader' if development?
-
-
-set :server, 'webrick'
+require 'twitter'
 
 Dotenv.load
 
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-
-client = Twitter::REST::Client.new do |config|
+$twitter_client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["CONSUMER_KEY"]
   config.consumer_secret    = ENV["CONSUMER_SECRET"]
   config.access_token        = ENV["ACCESS_TOKEN"]
   config.access_token_secret = ENV["ACCESS_SECRET"]
-
 end
 
 
-get '/twitter/:id' do 
-		tweet_array = []
-		client.user_timeline(params[:id].to_s, count: 20, lang: "en").collect do |tweet|
-			tweet_array << tweet.text.to_s 
+get '/twitter/:id' do
+	tweet_array = []
+	$twitter_client.user_timeline(params[:id].to_s, count: 20, lang: "en").collect do |tweet|
+		tweet_array << tweet.text.to_s
 	end
 	tweet_array.to_json
 end
-
 
 
 
--- public/main.js
+++ public/main.js
@@ -1,14 +1,27 @@
-window.onload = function() {
-	var submitButton = document.getElementById('submit');
-}
+$(document).ready(function() {
+  $('#submit').click(function(event){
+    event.preventDefault();
+    ajaxRequest($('#handle-field').val());
+  });
+});
 
 var render_tweets = function(tweets_json) {
-	var tweets_json = tweets_json;
-	tweets_html = ""
-	i = 0;
-	for (; i < tweets_json.length; i++) {
-		var tweet_text = tweets_json[i];
-		tweets_html += "<li>" + tweet_text + "</li>"
-	};
-	document.getElementById('tweets').innerHTML = tweets_html;
-}
+  var tweets_array = $.parseJSON(tweets_json)
+  tweets_html = ""
+  i = 0;
+  for (; i < tweets_array.length; i++){
+    var tweet_text = tweets_array[i];
+    tweets_html += "<li>" + tweet_text + "</li>"
+  };
+  $('#tweets').append(tweets_html);
+};
+
+var ajaxRequest = function(user) {
+  var url = '/twitter/' + user;
+  $.ajax({
+    url: url,
+    success: function(data) {
+      render_tweets(data);
+    } 
+  });
+};
