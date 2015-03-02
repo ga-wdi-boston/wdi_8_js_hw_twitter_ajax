e6f8617e7732028f02c543104a7e3f28f9f88066
abroccoli <acbrocco@syr.edu>
2015-03-02 18:50:40 -0500

    JSON from backend is displayed on the main page, date and tweet

--- static_client/app/scripts/main.js
+++ static_client/app/scripts/main.js
@@ -1,1 +1,32 @@
-console.log('\'Allo \'Allo!');
+var App = App || {};
+
+$(document).ready(function(){
+
+	$('#submit').on('click', function(event){
+		$twitterhandle = $('#userid').val();
+		App.getTweets(event, $twitterhandle);
+	});
+});
+
+
+App.getTweets = function(event, id){
+	$.ajax({
+		url: 'http://localhost:3000/tweets/'+id,
+		type: 'GET',
+	}).done(function(data){
+		App.displayTweets(data);
+	}).fail(function(){
+		$('#displayarea').html('Please enter a valid Twitter Handle');
+	});
+};
+
+App.displayTweets = function(data){
+	var html = '';
+	for(var i=0; i<data.tweets.length; i++){
+		var date = $.format.date(data.tweets[i].created_at, "MM/dd/yyyy");
+		html += '<div class="tweet"><h4>'+date+'</h4>'+data.tweets[i].text+'</div><br>'
+	}
+
+	$('#displayarea').html(html);
+};
+
