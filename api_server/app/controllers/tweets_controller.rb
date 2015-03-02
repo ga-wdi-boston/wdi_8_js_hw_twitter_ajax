a2d9950b7a5c7c7eaa6e067829a455172e968cb3
Charles Talbot <charlietalbot@gmail.com>
2015-03-02 17:58:46 -0500

    add twitter initializer

--- api_server/app/controllers/tweets_controller.rb
+++ api_server/app/controllers/tweets_controller.rb
@@ -1,4 +1,11 @@
 class Tweets < ApplicationController
+
+  # def index
+  # end
+
   def show
+    user_name = parmas[:id]
+    tweets = TwitterClient.user_timeline(user_name).map(&:text)
+    render json: tweet.to_json
   end
 end
