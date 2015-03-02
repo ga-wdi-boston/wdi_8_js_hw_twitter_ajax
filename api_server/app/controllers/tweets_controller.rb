01b10aa0863b5b4c67f15b246ae8fb4afbc26f2f
katyf <katy.feng.14@gmail.com>
2015-03-02 18:54:45 -0500

    homework done

---
+++ api_server/app/controllers/tweets_controller.rb
@@ -0,0 +1,10 @@
+class TweetsController < ApplicationController
+
+  def show
+    user_name = params[:id]
+    tweets = TwitterClient.user_timeline(user_name).map(&:text)
+    render json: { tweets: tweets }
+    #render html: tweets
+  end
+
+end
--- static_client/app/scripts/main.js
+++ static_client/app/scripts/main.js
@@ -1,1 +1,51 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+var TweetsApp = TweetsApp || {
+  $tweets: $('.tweetscontainer')
+};
+
+TweetsApp.init = function(){
+ var $search = $('input[id="search"]');
+ var $submitButton = $('#submitButton');
+
+  $submitButton.on('click', function(){
+    event.preventDefault();
+    var $username = $search.val();
+    console.log('button clicked');
+    TweetsApp.submitSearch($username);
+    //debugger;
+});
+};
+
+TweetsApp.submitSearch = function(username){
+  //if(event.preventDefault) event.preventDefault();
+  $.ajax({
+    url: 'http://localhost:3000/tweets/' + username,
+    type: 'GET',
+  })
+  .done(function(data) {
+    //console.log("success");
+    console.log(data);
+    data.tweets.reverse().forEach(TweetsApp.showTweets);
+  })
+  .fail(function() {
+    console.log("error");
+  });
+
+};
+
+
+TweetsApp.showTweets = function(tweet, index){
+  //if(event.preventDefault) event.preventDefault();
+  var $tweethtml = $('<div class=tweet id=' + index + '>');
+  $tweethtml.html('<p>' + tweet + '</p>');
+  TweetsApp.$tweets.prepend($tweethtml);
+
+};
+
+
+$(document).ready(function() {
+  console.log('hello!');
+  TweetsApp.init();
+
+});
