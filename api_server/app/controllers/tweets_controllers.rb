2097729621a8c34fdd2a10408fc2a70186a4924f
JC Gruta <jase.aturg@gmail.com>
2015-03-03 08:56:16 -0500

    Able to call tweets in the console. The GET is setup

--- api_server/app/controllers/tweets_controllers.rb
+++
@@ -1,7 +0,0 @@
-class TweetsController < ApplicationController
-  def show
-    user_name = params[:id]
-    tweet = TwitterClient.user_timeline(user_name)
-    render json: {tweets: tweet}
-  end
-end
--- static_client/app/scripts/main.js
+++ static_client/app/scripts/main.js
@@ -1,1 +1,15 @@
 console.log('\'Allo \'Allo!');
+
+var getTweets = function(){
+  $.ajax({
+    url:'http://localhost:3000/tweets/j_gruta',
+    type: 'GET'
+  }).done(function(tweets){
+    console.log(tweets);
+  });
+};
+
+
+$(document).ready(function(){
+  getTweets();
+});
