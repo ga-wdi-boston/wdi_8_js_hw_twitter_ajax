a240e9656366bb58169b23fff7aa44cd90a3065c
Kevin Whelan <Kevinwhelan2098@gmail.com>
2015-03-03 10:14:01 -0500

    help from this mornings review

--- api_server/app/controllers/tweets_controller.rb
+++ api_server/app/controllers/tweets_controller.rb
@@ -1,8 +1,10 @@
+require 'pry'
 class TweetsController < ApplicationController
   def show
     screenname = params[:id]
     tweets = TwitterClient.user_timeline(screenname).map(&:text)
-    rend json: { tweets: tweets }
+    binding.pry
+    rend json: { tweets: tweets }, status: 200
 
   end
 end
--- static_client/app/scripts/main.js
+++ static_client/app/scripts/main.js
@@ -1,37 +1,30 @@
+'use strict';
 console.log('\'Allo \'Allo!');
 
-TweetsApp = {};
-TweetsApp.getTweets = function(username){
+var TweetsApp = {};
+TweetsApp.getTweets = function(data){
   $.ajax({
-  url: 'http://localhost:3000/tweets/' + username,
+  url: 'http://localhost:3000/tweets/' + $('#user').val(),
   type: 'get',
  dataType: 'JSON',
-  }).done(TweetsApp.directTweets);
+  }).done(function(data){
+    TweetsApp.renderTweets(data);
+    console.log(data);
+
+})
+}
+
+
+TweetsApp.renderTweets = function(data){
+  for(var i = 0; i < data.tweets.length; i++){
+    var html = '<div>' + data.tweets[i].text + '</div>';
+    $('.row.marketing').append(html)
+  }
+
 };
 
-TweetsApp.Tweet = function(data){
-  this.id = data.id
-  this.text = data.text
-};
-
-TweetsApp.directTweets = function(data){
-    var tweet;
-   var html = '';
-  data.tweets.forEach(function(tweet){
-      tweet = new TweetsApp.Tweet(tweet);
-      html += TweetsApp.render(tweet);
-  })
-  $('#tweets').append(html)
-};
 
 
-
-TweetsApp.render = function(data){
-  html = '<div id="' + data.id + '">';
-  html += '<p>' + data.text + '</p>';
-  html += '</div>';
-  return html
-};
 
 $(document).ready(function(){
 
