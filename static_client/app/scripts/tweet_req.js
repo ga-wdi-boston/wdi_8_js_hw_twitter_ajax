0be9be59e892208c76bddb45e0b76513eb287ada
lady3bean <annehawleybrett@gmail.com>
2015-03-02 21:41:13 -0500

    returns the tweets with the creation of a tweet div that can be styled in proper styling files, rather than render html in the javascript (ugh!) - also changes index html for clearer form

--- static_client/app/scripts/tweet_req.js
+++ static_client/app/scripts/tweet_req.js
@@ -3,20 +3,22 @@
 var TwitterApp = TwitterApp || {};
 
 TwitterApp.tweetsByUser = {
-  get: function(username){
+  get: function ( username ) {
     $.ajax({
       url: 'http://localhost:3000/tweets/' + username,
       dataType: 'json'
-    }).then(function(data){
-      console.log(data);
-    }).fail(function(jqXHR, textStatus, ErrorThrown) {
-      console.log(jqXHR, textStatus, ErrorThrown);
-    });
-  }, 
+    })
+      .then(function getTweets ( data ) {
+        return data.tweets;
+      })
+      .then(TwitterApp.tweetsByUser.tweetHandler)
+      .fail(function ( jqXHR, textStatus, ErrorThrown ) {
+        console.log(jqXHR, textStatus, ErrorThrown);
+      });
+  },
+  tweetHandler: function ( remoteTweets ) {
+    $('.tweets-list').append(remoteTweets.map( function renderTweet ( tweet ) {
+      return $('<div class="tweet">').text(tweet);
+    }));
+  } 
 };
-
-// tweetHandler: function ( remoteTweets ) {
-  //   $('.tweets-list').append(remoteTweets.map( function renderTweet ( tweet ) {
-  //     return new TwitterApp.Tweet( tweet ).render();
-  //   }).join(''));
-  // }
