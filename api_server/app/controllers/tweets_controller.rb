7f78b5019cd637f60f615cdbcb8b02f6ffbbaff4
idelairre <idelairre@gmail.com>
2015-03-03 10:41:47 -0500

    added some of Pat's code and experimented with JSON output during the demo.

--- api_server/app/controllers/tweets_controller.rb
+++ api_server/app/controllers/tweets_controller.rb
@@ -3,6 +3,7 @@ class TweetsController < ApplicationController
     user = params[:id]
     # binding.pry
     tweets = TwitterClient.user_timeline(user).map(&:text)
+    # username = TwitterClient.user_timeline(user).map(&:screen_name)
     render json: {tweets: tweets}
   end
 end
