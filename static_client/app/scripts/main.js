ae5fb326e9e4142af02f3be6857130ea0156bbbd
Ross <rdegnen@gmail.com>
2015-03-02 21:43:07 -0500

    working on getting GET request to work

--- static_client/app/scripts/main.js
+++ static_client/app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+var App = App || {};
+
+App.getTweets = function(){
+  var $findTweets = $('#find-tweets').val();
+
+  $.ajax({
+    url: 'http://localhost:3000/tweets' + $findTweets,
+    type: 'GET',
+    dataType: 'JSON'
+  }).done(function(data){
+    console.log(data);
+  });
+};
+
+$(document).ready(function(){
+  $('#find-tweets').on('submit', function(event){
+    App.getTweets(event);
+  });
+});
