44196358ecb4dfcf2c65d8835d554875909517c0
brandonkoo <brandonkoo555@gmail.com>
2015-03-02 21:43:30 -0500

    modified main.js

--- static_client/app/scripts/main.js
+++ static_client/app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!');
+
+'use strict';
+
+console.log('\'hello \'hello!');
+
+
+
+var App = App || {};
+
+App.tweets = function(user_name){
+  var user_name = $('#user_name').val();
+  $.ajax({
+    url: "http://localhost:3000/tweets/" + user_name,
+    type: 'GET',
+    dataType: 'JSON',
+  });
+
+};
+
+
+$(document).ready(function(){
+  $( "#tweets" ).click(function() {
+  App.tweets($('#tweets').val());
+  });
+});
