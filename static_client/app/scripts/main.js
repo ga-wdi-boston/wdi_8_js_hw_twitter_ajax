85fbca62c2ce42c8f175bb3519d4f5892e5bdebe
Max Blaushild <blaushmild@gmail.com>
2015-03-02 23:05:56 -0500

    cosmetic changes to the static page and added a little functionality to the submit button, allowing you to submit multiple queries and not drown the page

--- static_client/app/scripts/main.js
+++ static_client/app/scripts/main.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
 
   $('#submit').on('click', function(){
     event.preventDefault();
+    $('#tweets').empty();
     var $value = $('#twitter-user').val()
     TwitterApp.getTweets($value);
   });
