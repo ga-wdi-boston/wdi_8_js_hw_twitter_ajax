e3ce03e1c0387452d0da0bde64192d4143c0615f
JC Gruta <jase.aturg@gmail.com>
2015-03-03 10:16:24 -0500

    Added the front end but couldn't get the app to render the tweets.

--- api_server/app/controllers/tweets_controller.rb
+++ api_server/app/controllers/tweets_controller.rb
@@ -3,9 +3,9 @@ require 'json'
 class TweetsController < ApplicationController
 
   def show
-  user_name = params[:id]
-    tweets = TwitterClient.user_timeline(user_name).take(20).map{|x| x.text}
+  user_name = params[:id] #shown in rake routes
+    tweets = TwitterClient.user_timeline(user_name)
     # render json: JSON.generate(tweets)
-    render json: {tweets: tweets}
+    render json: {tweets: tweets}, status: 200
   end
 end
--- static_client/app/scripts/main.js
+++ static_client/app/scripts/main.js
@@ -1,15 +1,28 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+var TwitterApp = TwitterApp || {};
 
-var getTweets = function(){
+$(document).ready(function(){
+  $('form#user-form').on('submit', function(event){
+    TwitterApp.getTweets(event);
+  });
+});
+
+TwitterApp.getTweets = function(event){
+  event.preventDefault();
   $.ajax({
-    url:'http://localhost:3000/tweets/j_gruta',
+    url:'http://localhost:3000/tweets/' + $('input#username').val(), // getting the value of whats entered in the input box
     type: 'GET'
-  }).done(function(tweets){
-    console.log(tweets);
+  }).done(function(data){
+    console.log(data);
   });
 };
 
+TwitterApp.renderTweets = function(data){
+  for (var i = 0; i < data.length; i++){
+    var html = '<div>' + data.tweets[i].text + '</div>';
+    $('.row.marketing').append(html);
+  }
+};
 
-$(document).ready(function(){
-  getTweets();
-});
+
+
