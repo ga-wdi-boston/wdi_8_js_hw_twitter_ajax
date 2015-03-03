5bf57a0fd0e7ee3b96493bcd4803551547c06d13
pat <patrick.h.needham@gmail.com>
2015-03-03 10:19:08 -0500

    some refactoring

--- api_server/app/controllers/tweets_controller.rb
+++ api_server/app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
 
   def show
     tweeter = params[:id]
-    tweets = TwitterClient.user_timeline(tweeter).map(&:text)
+    tweets = TwitterClient.user_timeline(tweeter)#.map(&:text)
     render json: {tweets: tweets}
   end
 end
