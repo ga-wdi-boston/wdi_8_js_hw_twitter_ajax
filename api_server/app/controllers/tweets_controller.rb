9a7d8add890c52467d9bba72da50b8d845db99d8
Charles Talbot <charlietalbot@gmail.com>
2015-03-03 08:39:41 -0500

    add tweets to page

--- api_server/app/controllers/tweets_controller.rb
+++ api_server/app/controllers/tweets_controller.rb
@@ -1,11 +1,20 @@
-class Tweets < ApplicationController
+class TweetsController < ApplicationController
+  def show
+    user_name = params[:id]
+    tweets = TwitterClient.user_timeline(user_name).map(&:text)
+    render json: {tweets: tweets}
+  end
 
-  # def index
+  # def create
+  #   current_user.tweet(twitter_params[:message])
   # end
 
-  def show
-    user_name = parmas[:id]
-    tweets = TwitterClient.user_timeline(user_name).map(&:text)
-    render json: tweet.to_json
-  end
+  # def twitter_params
+  #   params.require(:tweet).permit(:message)
+  # end
+
+  # def update
+  #   user_name = params[:id]
+  #   new_tweet = TwitterClient.update(tweet.to_s)
+  #   render json: {new_tweet: new_tweet}
 end
