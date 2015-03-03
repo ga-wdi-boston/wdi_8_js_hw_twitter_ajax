f9f5f76958b74587bb797252581b509621302ae7
Ross <rdegnen@gmail.com>
2015-03-02 22:45:57 -0500

    yaaaaaaaayyyy it worksgit statusgit statusgit statusgit status! im a geniusgit statusgit statusgit status

--- static_client/app/scripts/main.js
+++ static_client/app/scripts/main.js
@@ -1,14 +1,36 @@
 var App = App || {};
 
+App.Tweet = function(remoteTweet){
+  this.text = remoteTweet.text;
+};
+
+App.Tweet.prototype.render = function(){
+  var html = "<p>" + this.text + "</p>";
+  return html;
+};
+
+App.tweetHandler = function(remoteTweets){
+  var html = "<ul>", tweet;
+
+  remoteTweets.forEach(function(remoteTweet){
+    tweet = new App.Tweet(remoteTweet);
+    html += tweet.render();
+  })
+
+  html += "</ul>";
+  $('.tweets').append(html);
+};
+
 App.getTweets = function(){
   var $twitterUser = $('#twitter-username').val();
-  debugger;
+
   $.ajax({
     url: 'http://localhost:3000/tweets/' + $twitterUser,
     type: 'GET',
     dataType: 'JSON'
   }).done(function(data){
     console.log(data);
+    App.tweetHandler(data);
   });
 };
 
