167b38fb43eada39f231df2149ff7a6168f15927
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-03-04 21:03:40 -0500

    Add live search functionality

--- api_server/app/controllers/tweets_controller.rb
+++ api_server/app/controllers/tweets_controller.rb
@@ -1,9 +1,21 @@
+require 'pry'
 class TweetsController < ApplicationController
   def show
-    twitter_handle = params[:id]
+    #my code:
+    #twitter_handle = params[:id]
     #tweets = TwitterClient.user_timeline(twitter_handle) #returns all tweets
-    tweets = TwitterClient.user_timeline(twitter_handle).map(&:text) #returns just the text of all tweets
+    # tweets = TwitterClient.user_timeline(twitter_handle).map(&:text)
+
     #json wants keys & values
-    render json: {tweets: tweets}
+    # render json: {tweets: tweets}
+
+    #in class review
+    #http://localhost:3000/tweets/username
+    #tweets = TwitterClient.user_timeline(twitter_handle).map(&:text)
+# binding.pry
+    twitter_handle = params[:id]
+    tweets = TwitterClient.user_timeline(twitter_handle)
+    render json: {tweets: tweets}, status: 200
+
   end
 end
--- static_client/app/scripts/main.js
+++ static_client/app/scripts/main.js
@@ -1,24 +1,58 @@
-$(document).ready(function(){
+//My code
+// $(document).ready(function(){
 
+//   $.ajax({
+//     url: 'http://localhost:3000/tweets/beigesavage',
+//     type: 'GET'
+//   }).done(function(tweets){
+//     console.log("app: GET success");
+//     console.log("number of tweets: " + tweets.tweets.length);
+//     console.log(tweets);
+
+//     $('#tweets_div').append(tweets.tweets);
+
+//     //loop through elements in tweets array
+//     // tweets.forEach(function(tweets){
+//     //   $('.tweets_div').append('<p>' + tweet.tweets + '</p>');
+//     // });
+
+// // debugger;
+//   }).fail(function(jqXHR, textStatus, errorThrown){
+//     trace(jqXHR, textStatus, errorThrown);
+//   });
+
+// }); //end document ready
+
+/////////////////////////////////////////////////
+//In class review: using a twitter handle search field
+var App = App || {};
+
+App.getTweets = function(event){
+  event.preventDefault();
   $.ajax({
-    url: 'http://localhost:3000/tweets/beigesavage',
+    url: 'http://localhost:3000/tweets/' + $('input#username').val(),
     type: 'GET'
-  }).done(function(tweets){
-    console.log("app: GET success");
-    console.log("number of tweets: " + tweets.length);
-    console.log(tweets);
+  }).done(function(data){
+    console.log(data);
+    console.log(data.tweets.length);
+    App.renderTweets(data);
 
-    $('#tweets_div').append(tweets.tweets);
+  }).fail();
 
-    //loop through elements in tweets array
-    // tweets.forEach(function(tweets){
-    //   $('.tweets_div').append('<p>' + tweet.tweets + '</p>');
-    // });
+};//end App.getTweets
 
-// debugger;
-  }).fail(function(jqXHR, textStatus, errorThrown){
-    trace(jqXHR, textStatus, errorThrown);
+App.renderTweets = function(data){
+  for(var i=0; i < data.tweets.length; i++){
+    var html = '<div>' + data.tweets[i].text + '</div>';
+    $('.row.marketing').append(html);
+}
+
+};//end App.renderTweets
+
+$(document).ready(function(){
+  //capture the form submit
+  $('form#user-form').on('submit', function(event){
+    App.getTweets(event)
   });
-
 
 }); //end document ready
